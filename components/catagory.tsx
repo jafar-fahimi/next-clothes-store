10937fad1ebd4bb60792438fa6a7f6b0bf58fb69
jafar <jafar.fahimi12@gmail.com>
2023-02-04 22:18:37 +0430

    catagory.tsx added

--- components/catagory.tsx
+++ components/catagory.tsx
@@ -1,12 +1,21 @@
-type Props = { srcProp: string; cat: string; maxW: string };
+import Image, { StaticImageData } from "next/image";
+type Props = { srcProp: string | StaticImageData; cat: string };
 
-export default function Catagory({ srcProp, cat, maxW }: Props) {
+export default function Catagory({ srcProp, cat }: Props) {
   return (
-    <div className="flex justify-center align-middle">
-      <img src={srcProp} alt={cat} className="-z-10" />
-      <div className="flex flex-col bg-slate-400/50 px-8 py-4">
-        <span>{cat}</span>
-        <span>Shop Now</span>
+    <div>
+      <div className="relative">
+        <Image
+          src={srcProp}
+          alt={cat}
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -z-10 hover:scale-125"
+          width={300}
+          height={300}
+        />
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col bg-white/70 px-8 py-4 text-center">
+          <h3 className="text-xl font-bold">{cat}</h3>
+          <span className="text-lg">Shop Now</span>
+        </div>
       </div>
     </div>
   );
