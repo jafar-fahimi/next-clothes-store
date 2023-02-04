35e12190dd11e44f63236b43c4ba58a2649b55b8
jafar <jafar.fahimi12@gmail.com>
2023-02-04 18:56:11 +0430

    Navbar.tsx created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,6 @@
 import { Fragment, useState } from "react";
 import Head from "next/head";
-import nextLogo from "public/next.svg";
-import { menu, close } from "public/assets";
-
 function HomePage() {
-  const [active, setActive] = useState("Home");
-  const [toggle, setToggle] = useState(false);
-
   return (
     <Fragment>
       <Head>
@@ -15,53 +9,6 @@ function HomePage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="bg-slate-600 px-8">
-      <nav className="w-full flex py-6 justify-between items-center navbar">
-          <img src={nextLogo} alt="hoobank" className="w-[124px] h-[32px]" />
-          <ul className="list-none sm:flex hidden justify-end items-center flex-1 border-box text-lg lg:text-xl">
-            <li className="cursor-pointer mx-4 font-normal hover:font-bold focus:font-bold">
-              <a href="#">Shop</a>
-            </li>
-            <li className="cursor-pointer mx-4 font-normal hover:font-bold focus:font-bold">
-              <a href="#">Contact</a>
-            </li>
-            <li className="cursor-pointer mx-4 font-normal hover:font-bold focus:font-bold">
-              <a href="#">Sign In</a>
-            </li>
-            <li className="cursor-pointer mx-4 font-normal hover:font-bold focus:font-bold">
-              <a href="#">Icon/</a>
-            </li>
-          </ul>
-
-          <div className="sm:hidden flex flex-1 justify-end items-center">
-            <img
-              src={toggle ? close : menu}
-              alt="menu"
-              className="w-[28px] h-[28px] object-contain"
-              onClick={() => setToggle(!toggle)}
-            />
-
-            <div
-              className={`${
-                !toggle ? "hidden" : "flex"
-              } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
-            >
-              <ul className="list-none flex justify-end items-start flex-1 flex-col">
-                <li className="cursor-pointer mx-4 font-normal hover:font-bold focus:font-bold">
-                  <a href="#">Shop</a>
-                </li>
-                <li className="cursor-pointer mx-4 font-normal hover:font-bold focus:font-bold">
-                  <a href="#">Contact</a>
-                </li>
-                <li className="cursor-pointer mx-4 font-normal hover:font-bold focus:font-bold">
-                  <a href="#">Sign In</a>
-                </li>
-                <li className="cursor-pointer mx-4 font-normal hover:font-bold focus:font-bold">
-                  <a href="#">Icon/</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
       </main>
     </Fragment>
   );
