defbc08d6d4795c95112c19ce6af16b69f39f129
jafar <jafar.fahimi12@gmail.com>
2023-02-05 10:09:19 +0430

    Navbar Cart Icon corrected; span inside

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import nextLogo from "public/next.svg";
-import { menu, close } from "public/assets/";
+import { menu, close, bagIcon1, bagIcon2 } from "public/assets/";
 
 export default function Navbar() {
   const [toggle, setToggle] = useState(false);
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar ">
-      <Image src={nextLogo} alt="hoobank" width={100} height={50}/>
+      <Image src={nextLogo} alt="hoobank" width={100} height={50} />
       <ul className="list-none sm:flex hidden justify-end items-center flex-1 border-box text-lg lg:text-xl">
         <li className="cursor-pointer mx-4 font-normal hover:font-semibold focus:font-semibold">
           <a href="#">Shop</a>
@@ -20,7 +20,14 @@ export default function Navbar() {
           <a href="#">Sign In</a>
         </li>
         <li className="cursor-pointer mx-4 font-normal hover:font-semibold focus:font-semibold">
-          <a href="#">Icon</a>
+          <a href="#">
+            <div className="relative">
+              <Image src={bagIcon2} alt="shopping Cart" className="-z-10" width={25} />
+              <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 pt-2 text-sm">
+                0
+              </span>
+            </div>
+          </a>
         </li>
       </ul>
 
@@ -48,7 +55,14 @@ export default function Navbar() {
               <a href="#">Sign In</a>
             </li>
             <li className="cursor-pointer mx-4 font-normal hover:font-semibold focus:font-semibold">
-              <a href="#">Icon</a>
+              <a href="#">
+                <div className="relative">
+                  <Image src={bagIcon2} alt="shopping Cart" className="-z-10" width={25} />
+                  <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 pt-2 text-sm">
+                    0
+                  </span>
+                </div>
+              </a>
             </li>
           </ul>
         </div>
