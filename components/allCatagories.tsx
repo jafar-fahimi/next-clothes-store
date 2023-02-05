b1bb6d050e187c4b9f26067146812382a585b998
jafar <jafar.fahimi12@gmail.com>
2023-02-05 10:49:17 +0430

    allCatagories.tsx images style corrected

--- components/allCatagories.tsx
+++ components/allCatagories.tsx
@@ -4,14 +4,13 @@ import { brownHat, men, women, jacket, sneakers } from "/public/images/";
 
 export default function AllCatagories() {
   return (
-    <section className="flex flex-col">
-      <div className="flex flex-col flex-wrap sm:flex-row justify-evenly gap-y-2">
-        {/* <Catagory srcProp={INITIAL_STATE.sections[0].imageUrl} cat="Hat" /> */}
+    <section className="flex flex-col gap-y-4">
+      <div className="flex flex-col flex-wrap sm:flex-row justify-center gap-x-4 gap-y-4">
         <Catagory srcProp={brownHat} cat="Hat" />
         <Catagory srcProp={jacket} cat="Hat" />
         <Catagory srcProp={sneakers} cat="Hat" />
       </div>
-      <div className="flex flex-col flex-wrap sm:flex-row justify-evenly ">
+      <div className="flex flex-col flex-wrap sm:flex-row justify-center gap-x-4 gap-y-4">
         <Catagory srcProp={women} cat="Hat" />
         <Catagory srcProp={men} cat="Hat" />
       </div>
