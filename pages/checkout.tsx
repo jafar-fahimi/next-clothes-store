db825d27af4d77b0437325eed1f766e532876c07
jafar <jafar.fahimi12@gmail.com>
2023-02-05 16:22:03 +0430

    checkout initial pages coded

---
+++ pages/checkout.tsx
@@ -0,0 +1,14 @@
+export default function checkout() {
+  return (
+    <section className="max-w-4xl mx-auto">
+      <div className="flex justify-between my-4">
+        <span>Product</span>
+        <span>Description</span>
+        <span>Price</span>
+        <span>Quantity</span>
+        <span>Remove</span>
+      </div>
+      <hr />
+    </section>
+  );
+}
