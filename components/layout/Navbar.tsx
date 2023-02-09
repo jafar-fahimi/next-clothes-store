b0f030cdbd33d50bcd98d770c15bbba92ce5b995
jafar <jafar.fahimi12@gmail.com>
2023-02-09 18:03:53 +0430

    Navbar.tsx cartOverview div coded

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -2,10 +2,13 @@ import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import nextLogo from "public/next.svg";
-import { menu, close, bagIcon1, bagIcon2 } from "public/assets/";
+import { menu, close, bagIcon2 } from "public/assets/";
+import { brownHat, men, women, jacket, sneakers } from "public/images";
 
 export default function Navbar() {
   const [toggle, setToggle] = useState(false);
+  const [cartOverview, setCartOverview] = useState(false);
+  
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar ">
@@ -20,15 +23,42 @@ export default function Navbar() {
         <li className="cursor-pointer mx-4 font-normal hover:font-semibold focus:font-semibold">
           <a href="#">Sign In</a>
         </li>
-        <li className="cursor-pointer mx-4 font-normal hover:font-semibold focus:font-semibold">
-          <Link href="/checkout">
-            <div className="relative">
-              <Image src={bagIcon2} alt="shopping Cart" className="-z-10" width={25} />
-              <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 pt-2 text-sm">
-                0
-              </span>
+        <li className="relative mx-4 font-normal focus:font-semibold">
+          <div
+            onClick={() => setCartOverview(!cartOverview)}
+            className="hover:cursor-pointer  hover:font-semibold  relative"
+          >
+            <Image src={bagIcon2} alt="shopping Cart" className="-z-10" width={25} />
+            <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 pt-2 text-sm">
+              0
+            </span>
+          </div>
+          {cartOverview && (
+            <div className="absolute flex flex-col justify-between top-14 right-5 w-72 z-10 border-2 border-black bg-white p-4 ">
+              <div className="space-y-4 mb-8">
+                <div className="flex gap-x-4">
+                  <Image src={brownHat} width={70} alt="cart item image" />
+                  <div className="flex flex-col text-[18px]">
+                    <h4>Blue Bearnie</h4>
+                    <span>1 x 18$</span>
+                  </div>
+                </div>
+                <div className="flex gap-x-4">
+                  <Image src={brownHat} width={70} alt="cart item image" />
+                  <div className="flex flex-col text-[18px]">
+                    <h4>Blue Bearnie</h4>
+                    <span>1 x 18$</span>
+                  </div>
+                </div>
+              </div>
+              <Link
+                href="/checkout"
+                className="block uppercase px-8 w-full border-2 border-black py-4 hover:bg-black hover:text-white active:ring-4"
+              >
+                go to checkout
+              </Link>
             </div>
-          </Link>
+          )}
         </li>
       </ul>
 
