42f393873d97b0e57aa63c7a939aae918a8206af
jafar <jafar.fahimi12@gmail.com>
2023-02-10 18:04:48 +0430

    itemView simple example corrected

--- components/redux-toolkit/app/item/itemView.tsx
+++ components/redux-toolkit/app/item/itemView.tsx
@@ -14,27 +14,28 @@ export function ItemView() {
   const itemStateArray: Props[] = useSelector(
     (state: { item: { cartItems: Props[] } }) => state.item.cartItems
   );
+  console.log(itemStateArray);
   const iDispatch = useDispatch();
   return (
     <div>
       <h2>
         Item Names are: <br />
         {itemStateArray.map((itm) => (
-          <span key={itm.id}>{itm.name}</span>
+          <p key={itm.id}>{itm.name}</p>
         ))}
       </h2>
       <button
         className="bg-red-400 m-4"
         onClick={() => {
-          console.log("array of item obj is: ", itemStateArray);
           iDispatch(
             addToCart({
-              id: 1,
+              id: 10,
               name: "Brown Brim",
               imageUrl: "https://i.ibb.co/ZYW3VTp/brown-brim.png",
               price: 25,
             })
           );
+          console.log("array of item obj is: ", itemStateArray);
         }}
       >
         Order Item
