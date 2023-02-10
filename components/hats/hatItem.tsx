b35de42be8564b1130c6c1ab549b13ee73b9dad2
jafar <jafar.fahimi12@gmail.com>
2023-02-10 14:23:30 +0430

    hasItem.tsx add to cart Link hover fixed

--- components/hats/hatItem.tsx
+++ components/hats/hatItem.tsx
@@ -11,7 +11,7 @@ type Props = {
 };
 export default function HatItem({ shop_data_item }: Props) {
   return (
-    <div className="relative " key={shop_data_item.id}>
+    <div className="relative" key={shop_data_item.id}>
       <div className="w-64 h-80 -z-10 group">
         <Image
           alt={shop_data_item.name}
@@ -20,10 +20,9 @@ export default function HatItem({ shop_data_item }: Props) {
           height={300}
           src={shop_data_item.imageUrl}
         />
-        <div className="absolute top-0 bg-white/20 w-full h-full hidden group-hover:block z-10"></div>
-        <div className="absolute hidden group-hover:block -translate-x-1/2 -translate-y-1/2 bottom-10 left-1/2 text-black bg-white/90 hover:bg-black hover:text-white py-2 px-8 text-center">
-          <Link href="#" className="font-normal inline-block uppercase w-28">
-            Add To Cart
+        <div className="absolute invisible group-hover:visible -translate-x-1/2 -translate-y-1/2  bottom-10 left-1/2 text-black bg-white/90 cursor-pointer hover:bg-black hover:text-white text-center">
+          <Link href="#" className="block py-2 px-8 font-normal uppercase">
+            Add&nbsp;To&nbsp;Cart
           </Link>
         </div>
       </div>
