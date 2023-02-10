c548a72da438a6e55ad2bee57cfc4de633aa1c89
jafar <jafar.fahimi12@gmail.com>
2023-02-10 17:49:41 +0430

    oneItem.tsx coded

---
+++ components/layout/oneItem.tsx
@@ -0,0 +1,24 @@
+import Image, { StaticImageData } from "next/image";
+import React from "react";
+type Props = {
+  id: number;
+  name: string;
+  imageUrl: string | StaticImageData;
+  price: number;
+  qty?: number;
+  countInStock?: number;
+};
+
+export default function OneItem(props: Props) {
+  return (
+    <div className="flex gap-x-4">
+      <Image src={props.imageUrl} width={70} alt={props.name} />
+      <div className="flex flex-col text-[18px]">
+        <h4>{props.name}</h4>
+        <span>
+          {props.qty} x {props.price}
+        </span>
+      </div>
+    </div>
+  );
+}
