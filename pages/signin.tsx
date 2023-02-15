692f02b5a491a44d5013376566a3cb3f7991e865
jafar <jafar.fahimi12@gmail.com>
2023-02-15 20:33:39 +0430

    signin.tsx hover added

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -2,18 +2,27 @@ import React from "react";
 
 export default function Signin() {
   return (
-    <section className="flex flex-col md:flex-row md:justify-center mx-auto">
+    <section className="flex flex-col md:flex-row md:justify-center mx-auto gap-y-10 md:gap-y-0">
       <div className="w-full px-8">
         <h2 className="text-xl font-semibold">I already have an account</h2>
         <p className="text-sm mb-8 mt-1">Sign in with your email and password</p>
         <form action="#" method="post">
-          <input type="email" placeholder="Email" className="placeholder-slate-600  py-[2px] block w-full" />
+          <input type="email" placeholder="Email" className="placeholder-slate-600 py-[2px] block w-full" />
           <hr className="w-full mb-10" />
-          <input type="password" placeholder="Password" className="placeholder-slate-600  py-[2px] block w-full" />
-          <hr className="w-full mb-10" />
+          {/* <hr className="w-full mb-6" /> */}
+          <input
+            type="password"
+            placeholder="Password"
+            className="placeholder-slate-600  py-[2px] block w-full"
+          />
+          <hr className="w-full mb-6" />
           <div className="flex justify-between gap-x-4 mt-4">
-            <button className="flex-1 uppercase px-6 py-2 bg-black text-white">Sign in</button>
-            <button className="flex-1 uppercase px-6 py-2 bg-blue-600 text-white">Sign in with google</button>
+            <button className="flex-1 scale-90 sm:scale-100 uppercase box-border sm:px-6 py-4 bg-black text-white hover:text-black hover:bg-white border-2 border-transparent hover:border-black transition-all duration-300">
+              Sign in
+            </button>
+            <button className="flex-1 scale-90 sm:scale-100 uppercase box-border sm:px-6 py-4 bg-blue-600 text-white hover:text-blue-600 hover:bg-white border-2 border-transparent hover:border-blue-600 transition-all duration-300">
+              Sign in with google
+            </button>
           </div>
         </form>
       </div>
@@ -22,15 +31,29 @@ export default function Signin() {
         <p className="text-sm mb-8 mt-1">Sign in with your email and password</p>
 
         <form action="#" method="post">
-          <input type="text" placeholder="Display Name" className="placeholder-slate-600 block py-[2px] w-full" />
-          <hr className="w-full mb-4 md:mb-10"/>
+          <input
+            type="text"
+            placeholder="Display Name"
+            className="placeholder-slate-600 block py-[2px] w-full"
+          />
+          <hr className="w-full mb-4 md:mb-10" />
           <input type="email" placeholder="Email" className="placeholder-slate-600 block py-[2px] w-full" />
-          <hr className="w-full mb-4 md:mb-10"/>
-          <input type="password" placeholder="Password" className="placeholder-slate-600 block py-[2px] w-full" />
-          <hr className="w-full mb-4 md:mb-10"/>
-          <input type="password" placeholder="Confirm Password" className="placeholder-slate-600 block py-[2px] w-full" />
-          <hr className="w-full mb-4 md:mb-10"/>
-          <button className="uppercase px-10 py-2 bg-black text-white">Sign up</button>
+          <hr className="w-full mb-4 md:mb-10" />
+          <input
+            type="password"
+            placeholder="Password"
+            className="placeholder-slate-600 block py-[2px] w-full"
+          />
+          <hr className="w-full mb-4 md:mb-10" />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            className="placeholder-slate-600 block py-[2px] w-full"
+          />
+          <hr className="w-full mb-4 md:mb-10" />
+          <button className="uppercase px-16 py-4 bg-black text-white hover:text-black hover:bg-white border-2 border-transparent hover:border-black transition-all duration-300">
+            Sign up
+          </button>
         </form>
       </div>
     </section>
