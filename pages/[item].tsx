c2d618ccd4664645190427bc61c52a649826ccf5
jafar <jafar.fahimi12@gmail.com>
2023-02-15 07:50:20 +0430

    item/inex.tsx removed & [item].tsx coded

---
+++ pages/[item].tsx
@@ -0,0 +1,12 @@
+import ProductHome from "components/product/productHome";
+import { useRouter } from "next/router";
+
+export default function HatsPage() {
+  const router = useRouter();
+  console.log("router is ", router.query);
+  type dataProp = { item: "hats" | "jackets" | "sneakers" | "men" | "women" };
+  const item: any = router.query.item as unknown as dataProp;
+  // const { item }: dataProp = data;
+
+  return <ProductHome pitem={item} />;
+}
