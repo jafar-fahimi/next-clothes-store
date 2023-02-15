56dc9caca1d2a0e03db7d42f92acfc14734ce8e1
jafar <jafar.fahimi12@gmail.com>
2023-02-15 07:50:47 +0430

    pitem-is-undefined error solved in productHome.tsx

--- components/product/productHome.tsx
+++ components/product/productHome.tsx
@@ -5,12 +5,14 @@ type Props = {
   pitem: "jackets" | "hats" | "sneakers" | "men" | "women";
 };
 export default function ProductHome({ pitem }: Props) {
-  //   console.log("pitem is:", pitem, pitem === "hats");
+  // console.log("pitem is:", pitem, pitem === "hats");
+  const pitem2 = pitem || "hats"; // just to avoid pitem-is-undefined error; exist in initial-load
+
   return (
     <section>
-      <h1 className="text-3xl text-center font-semibold mb-8">{SHOP_DATA[pitem].title}</h1>
+      <h1 className="text-3xl text-center font-semibold mb-8">{SHOP_DATA[pitem2].title}</h1>
       <div className="flex flex-wrap gap-3 justify-center">
-        {SHOP_DATA[pitem].items.map((shop_data_item) => (
+        {SHOP_DATA[pitem2].items.map((shop_data_item) => (
           <ProductItem key={shop_data_item.id} shop_data_item={shop_data_item} />
         ))}
       </div>
