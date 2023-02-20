526ebd7f94ec253bf345097fd4fe1e18202dc33c
jafar <jafar.fahimi12@gmail.com>
2023-02-20 22:00:16 +0430

    signup form, signUp coded

--- components/user/signup.tsx
+++ components/user/signup.tsx
@@ -1,5 +1,102 @@
-import React from "react";
+import { AuthError, createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "firebaseApp";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type Inputs = {
+  email: string;
+  password: string;
+};
 
 export default function SignUp() {
-  return <div>signUp</div>;
+  const [localLoading, setLocalLoading] = useState(false);
+  const [error, setError] = useState<null | AuthError>(null);
+  const [matchPasswordErr, setMatchPasswordErr] = useState(false);
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const signUp = async (email: string, password: string) => {
+    setLocalLoading(true);
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        router.push("/");
+      })
+      .catch((err) => {
+        alert(err.message);
+        setError(err.message);
+      })
+      .finally(() => setLocalLoading(false));
+  };
+
+  const onSubmitSignUp: SubmitHandler<Inputs> = async ({ email, password }) => {
+    await signUp(email, password);
+  };
+  if (localLoading) return <h3 className="text-3xl">Loading...</h3>;
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmitSignUp)} action="#" method="post">
+        <input
+          type="text"
+          placeholder="Display Name"
+          className={` placeholder-slate-600 py-[2px] border-b-2 block w-full lg:text-xl outline-none `}
+          required
+        />
+        <p className="py-2 mb-4 text-[13px] font-light text-orange-500"></p>
+        <input
+          type="email"
+          placeholder="Email"
+          onKeyDown={() => setError(null)}
+          className={` placeholder-slate-600 py-[2px] border-b-2 block w-full lg:text-xl outline-none ${
+            errors.email && "border-orange-500"
+          }`}
+          {...register("email", { required: true })}
+        />
+        <p className="py-2 mb-4 text-[13px] font-light text-orange-500">
+          {errors.email && <span className="absolute">Please enter a valid email.</span>}
+        </p>
+
+        <input
+          type="password"
+          placeholder="Password"
+          onKeyDown={() => setError(null)}
+          className={`placeholder-slate-600 border-b-2 py-[2px] block w-full lg:text-xl outline-none ${
+            errors.password && "border-orange-500"
+          }`}
+          {...register("password", {
+            required: true,
+            minLength: 4,
+            maxLength: 60,
+          })}
+        />
+        <p className="py-2 mb-8 text-[13px] font-light text-orange-500">
+          {errors.password && (
+            <span className="absolute"> Your password must contain between 4 and 60 characters.</span>
+          )}
+        </p>
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          required
+          onKeyDown={() => setMatchPasswordErr(false)}
+          className={`placeholder-slate-600 border-b-2 py-[2px] block w-full lg:text-xl outline-none ${
+            errors.password && "border-orange-500"
+          }`}
+        />
+        <p className="py-2 mb-8 text-[13px] font-light text-orange-500">
+          {matchPasswordErr && (
+            <span className="absolute">Your confirm password must match your first password.</span>
+          )}
+        </p>
+        <button className="uppercase px-16 py-4 bg-black text-white hover:text-black hover:bg-white border-2 border-transparent hover:border-black transition-all duration-300">
+          Sign up
+        </button>
+      </form>
+    </div>
+  );
 }
