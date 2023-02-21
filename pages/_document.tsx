7c8c4b6890fb4ee9b4aca757367e32b72a8e0691
jafar <jafar.fahimi12@gmail.com>
2023-02-21 10:48:15 +0430

    _document.tsx absolute pos

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en" className="top-0 left-0 w-full -translate-x-1/2">
+      <Html lang="en" className="absolute top-0 left-1/2 w-full -translate-x-1/2">
         <Head />
         <body>
           <Main />
