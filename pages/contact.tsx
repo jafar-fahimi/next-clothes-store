ecdda3ea553927f92ffcf663446e855ebd77da59
jafar <jafar.fahimi12@gmail.com>
2023-02-21 12:45:56 +0430

    contact.tsc colorful gradient bg removed

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -11,7 +11,7 @@ export default function Example() {
 
   return (
     <section className="isolate bg-white py-4 px-6 sm:py-12 lg:px-8">
-      <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
+      {/* <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
         <svg
           className="relative left-1/2 -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-40rem)] sm:h-[42.375rem]"
           viewBox="0 0 1155 678"
@@ -35,7 +35,7 @@ export default function Example() {
             </linearGradient>
           </defs>
         </svg>
-      </div>
+      </div> */}
       <div className="mx-auto max-w-2xl text-center">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact sales</h2>
         <p className="mt-2 text-lg leading-8 text-gray-600">
