9f9f3da3fa82b794a8fc82810c18865512a044a8
jafar <jafar.fahimi12@gmail.com>
2023-02-23 17:03:47 +0430

    index.tsx [item].tsx comments deleted

--- pages/[item].tsx
+++ pages/[item].tsx
@@ -1,6 +1,4 @@
 import ProductHome from "components/product/productHome";
-import { NextPageContext } from "next";
-import { StaticImageData } from "next/image";
 import { useRouter } from "next/router";
 import Stripe from "stripe";
 import { PriceProps, ProductProps } from "utils/types";
@@ -9,35 +7,12 @@ export default function HatsPage({ myPrices }: { myPrices: PriceProps[] }) {
   const router = useRouter();
   console.log("prices is : ", myPrices); // arr of obj each has product!
 
-  // console.log("[item] prices is ", prices);
-  // console.log(
-  //   "[item] prices product are: ",
-  //   prices.map((p) => p.product)
-  // );
-
   type dataProp = { item: "hats" | "jackets" | "sneakers" | "men" | "women" };
   const item: any = router.query.item as unknown as dataProp;
   // const { item }: dataProp = data;
 
   return <ProductHome pitem={item} data={myPrices} />;
 }
-
-// export async function getServerSideProps() {
-//   const clientStripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {});
-//   const clientStripedata = await clientStripe;
-
-//   console.log("clientStipeData: ", clientStripedata);
-
-//   const { data: prices } = await clientStripe.prices.list({
-//     active: true,
-//     expand: ["data.product"],
-//   });
-//   return {
-//     props: {
-//       prices,
-//     },
-//   };
-// }
 
 export async function getStaticProps(context: any) {
   const { params } = context;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,9 @@
 import React from "react";
 import Head from "next/head";
 import AllCatagories from "components/catagory/allCatagories";
-import Stripe from "stripe";
 import Catagory from "components/catagory/catagory";
-import { StaticImageData } from "next/image";
-
-// type ProductProps = {
-//   active: boolean;
-//   attributes: [];
-//   created: number;
-//   default_price: string;
-//   description: string;
-//   id: string;
-//   images: string[] | StaticImageData[];
-//   livemode: boolean;
-//   metadata: {};
-//   name: string;
-//   object: string;
-//   package_dimensions: null | string;
-//   shippable: null | string;
-//   statement_descriptor: null | string;
-//   tax_code: null | string | number;
-//   type: string;
-//   unit_label: null | string | number;
-//   updated: number;
-//   url: null | string;
-// };
-// type PriceProps = {
-//   active: boolean;
-//   billing_scheme: string;
-//   created: 1676975438;
-//   currency: string;
-//   custom_unit_amount: null | string;
-//   id: string;
-//   livemode: false;
-//   lookup_key: null;
-//   metadata: {};
-//   nickname: null | string;
-//   object: string;
-//   product: ProductProps;
-//   recurring: null;
-//   tax_behavior: string;
-//   tiers_mode: null | string;
-//   transform_quantity: null | string;
-//   type: string;
-//   unit_amount: number;
-//   unit_amount_decimal: string;
-// };
 
 function HomePage() {
-  // const productsArray = prices.map((price: PriceProps) => price.product);
-  // console.log("product2: ", productsArray);
-
   return (
     <React.Fragment>
       <Head>
@@ -66,20 +18,3 @@ function HomePage() {
   );
 }
 export default HomePage;
-
-// export async function getServerSideProps() {
-//   const clientStripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {});
-//   const clientStripedata = await clientStripe;
-
-//   console.log(clientStripedata, "clientStipeData");
-
-//   const { data: prices } = await clientStripe.prices.list({
-//     active: true,
-//     expand: ["data.product"],
-//   });
-//   return {
-//     props: {
-//       prices,
-//     },
-//   };
-// }
