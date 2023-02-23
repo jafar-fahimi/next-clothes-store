66bce18b0081925bb08ac6c7c2f8361e55e7e6f0
jafar <jafar.fahimi12@gmail.com>
2023-02-23 17:03:22 +0430

    productHome.tsx itemData coded

--- components/product/productHome.tsx
+++ components/product/productHome.tsx
@@ -1,26 +1,28 @@
-import { StaticImageData } from "next/image";
 import SHOP_DATA from "utils/shop.data";
 import { PriceProps } from "utils/types";
 import ProductItem from "./productItem";
-
 
 type Props = {
   pitem: "jackets" | "hats" | "sneakers" | "men" | "women";
   data: PriceProps[];
 };
-export default function ProductHome({ pitem, data }: Props) {
+export default function ProductHome({ pitem, data = [] }: Props) {
   // console.log("pitem is:", pitem, pitem === "hats");
   const pitem2 = pitem || "hats"; // just to avoid pitem-is-undefined error; exist in initial-load
   // took near 10 hours!
+
   console.log("data in productHome is: ", data);
+  const itemData: PriceProps[] | [] = data.filter((d) => d.product.description.includes("watch"));
+  console.log("item Data is", itemData); // watch freepik
 
   return (
     <section>
       <h1 className="text-3xl text-center font-semibold mb-8">{SHOP_DATA[pitem2].title}</h1>
       <div className="flex flex-wrap gap-5 gap-y-9 justify-center">
-        {SHOP_DATA[pitem2].items.map((shop_data_item) => (
+
+        {/* {SHOP_DATA[pitem2].items.map((shop_data_item) => (
           <ProductItem key={shop_data_item.id} shop_data_item={shop_data_item} />
-        ))}
+        ))} */}
       </div>
     </section>
   );
