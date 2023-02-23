3debf2255c9e4d31ac5ac920e5dcff2bb1c21b04
jafar <jafar.fahimi12@gmail.com>
2023-02-23 18:31:03 +0430

    bags, watches img

--- pages/[item].tsx
+++ pages/[item].tsx
@@ -3,22 +3,26 @@ import { useRouter } from "next/router";
 import Stripe from "stripe";
 import { PriceProps, ProductProps } from "utils/types";
 
-export default function HatsPage({ myPrices }: { myPrices: PriceProps[] }) {
+let myPaths: any = [];
+
+export default function HatsPage({ myPrices = [] }: { myPrices: PriceProps[] }) {
   const router = useRouter();
-  console.log("prices is : ", myPrices); // arr of obj each has product!
+  // console.log("myPrices in [item] is : ", myPrices); // arr of obj each has product!
 
   type dataProp = { item: "hats" | "jackets" | "sneakers" | "men" | "women" };
   const item: any = router.query.item as unknown as dataProp;
-  // const { item }: dataProp = data;
+
+  for (let p = 0; p < 30; p++) {
+    myPaths.push({ params: { item: (p + 1).toString } });
+  }
 
   return <ProductHome pitem={item} data={myPrices} />;
 }
 
 export async function getStaticProps(context: any) {
   const { params } = context;
-  console.log(`Generating page for /products/${params.item}`);
-
   const clientStripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {});
+
   // const clientStripedata = await clientStripe;
   // console.log("clientStipeData: ", clientStripedata);
 
@@ -38,7 +42,7 @@ export async function getStaticProps(context: any) {
 
 export async function getStaticPaths() {
   return {
-    paths: [{ params: { item: "1" } }],
+    paths: myPaths,
     fallback: true,
   };
 }
