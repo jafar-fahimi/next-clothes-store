8d3d5d7df1b59b2aa1f432dbb330f0728773c36e
jafar <jafar.fahimi12@gmail.com>
2023-02-23 18:29:58 +0430

    productHome itemData comes from data Prop

--- components/product/productHome.tsx
+++ components/product/productHome.tsx
@@ -11,15 +11,23 @@ export default function ProductHome({ pitem, data = [] }: Props) {
   const pitem2 = pitem || "hats"; // just to avoid pitem-is-undefined error; exist in initial-load
   // took near 10 hours!
 
-  console.log("data in productHome is: ", data);
-  const itemData: PriceProps[] | [] = data.filter((d) => d.product.description.includes("watch"));
-  console.log("item Data is", itemData); // watch freepik
+  const itemData: PriceProps[] = data.filter((d) => d.product.description.includes(pitem));
+  // console.log(`produtcHome pitem is: ${pitem} ${typeof pitem}, itemData is : ${itemData}`);
+  // console.log("data is ,", data);
 
   return (
     <section>
       <h1 className="text-3xl text-center font-semibold mb-8">{SHOP_DATA[pitem2].title}</h1>
       <div className="flex flex-wrap gap-5 gap-y-9 justify-center">
-
+        {itemData.map((itemD) => (
+          <ProductItem
+            id={+itemD.product.id}
+            key={itemD.product.id}
+            name={itemD.product.name}
+            price={itemD.unit_amount.toFixed(1)}
+            imageUrl={itemD.product.images[0]}
+          />
+        ))}
         {/* {SHOP_DATA[pitem2].items.map((shop_data_item) => (
           <ProductItem key={shop_data_item.id} shop_data_item={shop_data_item} />
         ))} */}
