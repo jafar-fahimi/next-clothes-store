2e8f20cd980643b5aa37b0360b8b7ae6fdf55782
jafar <jafar.fahimi12@gmail.com>
2023-02-23 16:03:17 +0430

    catagory.tsx width,height added to Image

--- components/catagory/catagory.tsx
+++ components/catagory/catagory.tsx
@@ -11,6 +11,8 @@ export default function Catagory({ srcProp, cat }: Props) {
           className="group-hover:scale-105 transition-all w-full h-full duration-500 flex-1"
           src={srcProp}
           quality={100}
+          width={100}
+          height={80}
         />
         <div className="absolute hover:cursor-pointer -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-40 bg-white/70 hover:bg-white/90 transition-all duration-300 py-4 px-2 text-center">
           <h3 className="md:text-xl">{cat}</h3>
