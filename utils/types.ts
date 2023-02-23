f1aaec86df9fdcdbc0c50c32cd8fde1a7121f883
jafar <jafar.fahimi12@gmail.com>
2023-02-23 16:50:15 +0430

    utils/types.ts; PriceProps, ProductProps for stripe data created

---
+++ utils/types.ts
@@ -0,0 +1,45 @@
+import { StaticImageData } from "next/image";
+
+export type PriceProps = {
+  active: boolean;
+  billing_scheme: string;
+  created: 1676975438;
+  currency: string;
+  custom_unit_amount: null | string;
+  id: string;
+  livemode: false;
+  lookup_key: null;
+  metadata: {};
+  nickname: null | string;
+  object: string;
+  product: ProductProps;
+  recurring: null;
+  tax_behavior: string;
+  tiers_mode: null | string;
+  transform_quantity: null | string;
+  type: string;
+  unit_amount: number;
+  unit_amount_decimal: string;
+};
+
+export type ProductProps = {
+  active: boolean;
+  attributes: [];
+  created: number;
+  default_price: string;
+  description: string;
+  id: string;
+  images: string[] | StaticImageData[];
+  livemode: boolean;
+  metadata: {};
+  name: string;
+  object: string;
+  package_dimensions: null | string;
+  shippable: null | string;
+  statement_descriptor: null | string;
+  tax_code: null | string | number;
+  type: string;
+  unit_label: null | string | number;
+  updated: number;
+  url: null | string;
+};
