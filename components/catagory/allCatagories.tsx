a4a4b301afe57e4a296b9c1a6ec5ad4dd8598907
jafar <jafar.fahimi12@gmail.com>
2023-02-25 11:58:42 +0430

    allCatagories; men,women cat changed to watches, bags

--- components/catagory/allCatagories.tsx
+++ components/catagory/allCatagories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Catagory from "./catagory";
-import { brownHat, men, women, jacket, sneakers } from "public/images";
+import { brownHat, rolexwatches, bagCollection, jacket, sneakers } from "public/images";
 
 export default function AllCatagories() {
   return (
@@ -11,8 +11,8 @@ export default function AllCatagories() {
         <Catagory srcProp={sneakers} cat="Sneakers" />
       </div>
       <div className="flex flex-col items-center w-full flex-wrap sm:flex-row justify-center gap-x-4 gap-y-4">
-        <Catagory srcProp={women} cat="Women" />
-        <Catagory srcProp={men} cat="Men" />
+        <Catagory srcProp={rolexwatches} cat="watches" />
+        <Catagory srcProp={bagCollection} cat="bags" />
       </div>
     </section>
   );
