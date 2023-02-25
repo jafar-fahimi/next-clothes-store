1215f1a945b3d3e82f3fe95c194c1892f7cc2182
jafar <jafar.fahimi12@gmail.com>
2023-02-25 12:14:29 +0430

    [item].tsx HatsPage moved to bottom

--- pages/[item].tsx
+++ pages/[item].tsx
@@ -1,11 +1,43 @@
 import ProductHome from "components/product/productHome";
+import { GetStaticProps, NextPage } from "next";
 import { useRouter } from "next/router";
 import Stripe from "stripe";
 import { PriceProps, ProductProps } from "utils/types";
 
 let myPaths: any = [];
+type Props = {
+  prices: PriceProps[];
+};
 
-export default function HatsPage({ myPrices = [] }: { myPrices: PriceProps[] }) {
+export const getStaticProps: GetStaticProps = async (context: any) => {
+  const { params } = context;
+  // const clientStripe = new Stripe( apiKey: process.env.STRIPE_SECRET_KEY ?? "", config: {apiVersion:"2020-08-27"} );
+  const clientStripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {});
+
+  const { data: prices } = await clientStripe.prices.list({
+    active: true, // ignore archive products!
+    limit: 20,
+    expand: ["data.product"],
+  });
+  const { prices: myPrices } = { prices };
+
+  return {
+    props: {
+      prices: myPrices,
+    },
+    revalidate: 10,
+  };
+};
+
+export async function getStaticPaths() {
+  return {
+    paths: myPaths,
+    fallback: true,
+  };
+}
+
+// export default function HatsPage({ myPrices = [] }: { myPrices: PriceProps[] }) {
+const HatsPage: NextPage<Props> = ({ prices }: Props) => {
   const router = useRouter();
   // console.log("myPrices in [item] is : ", myPrices); // arr of obj each has product!
 
@@ -15,34 +47,8 @@ export default function HatsPage({ myPrices = [] }: { myPrices: PriceProps[] }) 
   for (let p = 0; p < 30; p++) {
     myPaths.push({ params: { item: (p + 1).toString } });
   }
+  console.log("prices is : ", prices);
+  return <ProductHome pitem={item} data={prices} />;
+};
 
-  return <ProductHome pitem={item} data={myPrices} />;
-}
-
-export async function getStaticProps(context: any) {
-  const { params } = context;
-  const clientStripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {});
-
-  // const clientStripedata = await clientStripe;
-  // console.log("clientStipeData: ", clientStripedata);
-
-  const { data: prices } = await clientStripe.prices.list({
-    active: true,
-    expand: ["data.product"],
-  });
-  const { prices: myPrices } = { prices };
-
-  return {
-    props: {
-      myPrices,
-    },
-    revalidate: 10,
-  };
-}
-
-export async function getStaticPaths() {
-  return {
-    paths: myPaths,
-    fallback: true,
-  };
-}
+export default HatsPage;
