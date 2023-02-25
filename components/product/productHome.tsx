4e85de4a32ae9c896e3679fc639733e868601786
jafar <jafar.fahimi12@gmail.com>
2023-02-25 21:32:22 +0430

    productHome.tsx pitem2 uncommented & added in title

--- components/product/productHome.tsx
+++ components/product/productHome.tsx
@@ -8,7 +8,7 @@ type Props = {
 };
 export default function ProductHome({ pitem, data = [] }: Props) {
   // console.log("pitem is:", pitem, pitem === "hats");
-  // const pitem2 = pitem; // just to avoid pitem-is-undefined error; exist in initial-load
+  const pitem2 = pitem || "items"; // just to avoid pitem-is-undefined error; exist in initial-load
   // took near 10 hours!
 
   const itemData: PriceProps[] = data.filter((d) => d.product.description.includes(pitem));
@@ -16,14 +16,14 @@ export default function ProductHome({ pitem, data = [] }: Props) {
 
   return (
     <section>
-      <h1 className="text-3xl text-center font-semibold mb-8">{pitem.toUpperCase()}</h1>
+      <h1 className="text-3xl text-center font-semibold mb-8">{pitem2.toUpperCase()}</h1>
       <div className="flex flex-wrap gap-5 gap-y-9 justify-center">
         {itemData.map((itemD) => (
           <ProductItem
-            id={+itemD.product.id}
+            id={itemD.product.id}
             key={itemD.product.id}
             name={itemD.product.name}
-            price={itemD.unit_amount.toFixed(1)}
+            price={+itemD.unit_amount}
             imageUrl={itemD.product.images[0]}
           />
         ))}
