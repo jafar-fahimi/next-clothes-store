54eb2b58023b851cdc07bd2e4904cde509ca247b
jafar <jafar.fahimi12@gmail.com>
2023-02-25 12:13:32 +0430

    productHome.tsx SHOP_DATA title completely changed to static

--- components/product/productHome.tsx
+++ components/product/productHome.tsx
@@ -1,23 +1,22 @@
-import SHOP_DATA from "utils/shop.data";
+// import SHOP_DATA from "utils/shop.data";
 import { PriceProps } from "utils/types";
 import ProductItem from "./productItem";
 
 type Props = {
-  pitem: "jackets" | "hats" | "sneakers" | "men" | "women";
+  pitem: "jackets" | "hats" | "sneakers" | "bags" | "watches";
   data: PriceProps[];
 };
 export default function ProductHome({ pitem, data = [] }: Props) {
   // console.log("pitem is:", pitem, pitem === "hats");
-  const pitem2 = pitem || "hats"; // just to avoid pitem-is-undefined error; exist in initial-load
+  // const pitem2 = pitem; // just to avoid pitem-is-undefined error; exist in initial-load
   // took near 10 hours!
 
   const itemData: PriceProps[] = data.filter((d) => d.product.description.includes(pitem));
   // console.log(`produtcHome pitem is: ${pitem} ${typeof pitem}, itemData is : ${itemData}`);
-  // console.log("data is ,", data);
 
   return (
     <section>
-      <h1 className="text-3xl text-center font-semibold mb-8">{SHOP_DATA[pitem2].title}</h1>
+      <h1 className="text-3xl text-center font-semibold mb-8">{pitem.toUpperCase()}</h1>
       <div className="flex flex-wrap gap-5 gap-y-9 justify-center">
         {itemData.map((itemD) => (
           <ProductItem
@@ -28,9 +27,6 @@ export default function ProductHome({ pitem, data = [] }: Props) {
             imageUrl={itemD.product.images[0]}
           />
         ))}
-        {/* {SHOP_DATA[pitem2].items.map((shop_data_item) => (
-          <ProductItem key={shop_data_item.id} shop_data_item={shop_data_item} />
-        ))} */}
       </div>
     </section>
   );
