07c8b4328db3b59c63de603cd6689ef3e6989b54
jafar <jafar.fahimi12@gmail.com>
2023-02-26 22:55:24 +0430

    productItem <Image h-full del

--- components/product/productItem.tsx
+++ components/product/productItem.tsx
@@ -28,12 +28,12 @@ export default function ProductItem(props: RealProps) {
 
   return (
     <div className="relative" key={shop_data_item.id}>
-      <div className="w-64 h-80 -z-10 group">
+      <div className="w-64 -z-10 group">
         <Image
           alt={shop_data_item.name}
-          className="w-full h-full"
+          className="w-full"
           width={300}
-          height={300}
+          height={100}
           src={shop_data_item.imageUrl}
         />
         <div className="absolute invisible group-hover:visible -translate-x-1/2 -translate-y-1/2  bottom-10 left-1/2 transition-all duration-200 text-black bg-white/90 cursor-pointer hover:bg-black hover:text-white text-center">
