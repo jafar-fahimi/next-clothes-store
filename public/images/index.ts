b40960d2bdc3578a93303e98c569386cb7ddc703
jafar <jafar.fahimi12@gmail.com>
2023-02-26 22:58:03 +0430

    public/images/index product img

--- public/images/index.ts
+++ public/images/index.ts
@@ -19,8 +19,73 @@ import jacket from "./Home-Images/jackets.png";
 import sneakers from "./Home-Images/sneakers.png";
 import rolexwatches from "./Home-Images/rolex-watches.jpg";
 import bagCollection from "./Home-Images/bags.jpeg";
+// images of product folder:
+import blackToteBag from "./products/bags/Black tote bag.png";
+import capciusFemalePurse from "./products/bags/Capcius female purse.png";
+import cottonToteBags from "./products/bags/cotton tote bags.png";
+import eleganceYellowBag from "./products/bags/elegance yellow bag.png";
+import femaleHandBag from "./products/bags/female hand bag.png";
+import leatherHandBag from "./products/bags/leather hand bag.png";
+import acessoryTraditional from "./products/hats/Acessory Traditional Clothing.png";
+import backgroundHatVintageFemale from "./products/hats/Background Hat Vintage Female.png";
+import backgroundHatsLadiesClothing from "./products/hats/Background Hats Ladies Clothing Sun.png";
+import canvasHatBlank from "./products/hats/Canvas Hat Blank Fasion Sport.png";
+import fasionBeautySunwear from "./products/hats/Fasion Beauty Sunwear.png";
+import greatCapFasion from "./products/hats/Great Cap fasion white elegance.png";
+import frontViewOfHoodie from "./products/jackets/Front View of Hoodie Jeans.png";
+import orangeJacket from "./products/jackets/Orange Jacket.png";
+import stillLifeRenderBrown from "./products/jackets/Still Life Rendering of Brown jacket display.png";
+import stillLifeRenderFront from "./products/jackets/Still Life Rendering of jacket Front display.png";
+import stillLifeRenderJacketFront2 from "./products/jackets/Still Life Rendering of jacket Front display 2.png";
+import stillLifeRenderRedJacket from "./products/jackets/Still Life Rendering of Red jacket display.png";
+import aPairOfTrainers from "./products/sneakers/a-pair-of-trainers.png";
+import fashionShoe from "./products/sneakers/Fashion-Shoe.png";
+import fasionShoeAndSneakers from "./products/sneakers/fashion-shoes-and-sneakers.png";
+import fasionShoeAndSneakers2 from "./products/sneakers/fasion-shoes-and-sneakers-(2).png";
+import femaleSneakersRun from "./products/sneakers/Female-Sneakers-for-run.png";
+import newPairWhiteSne from "./products/sneakers/New-pair-of white-sneakers-2.png";
+import pairWork from "./products/sneakers/pair of workout sneakers on white background.png";
+import unbrandedModern from "./products/sneakers/unbranded-modern-sporty-shoe.png";
+import birthdayWatch from "./products/watches/birthday-watch.png";
+import collectionWatch from "./products/watches/collection-of-watch.png";
+import collectionWatches from "./products/watches/collection-of-watches.png";
+import femaleWatch from "./products/watches/female-watch.png";
+import pocketWatch from "./products/watches/pocket-watch.png";
+import watchShow from "./products/watches/watch-show.png";
 
 export {
+  backgroundHatVintageFemale,
+  backgroundHatsLadiesClothing,
+  canvasHatBlank,
+  fasionBeautySunwear,
+  greatCapFasion,
+  frontViewOfHoodie,
+  orangeJacket,
+  stillLifeRenderBrown,
+  stillLifeRenderFront,
+  stillLifeRenderJacketFront2,
+  stillLifeRenderRedJacket,
+  aPairOfTrainers,
+  fashionShoe,
+  fasionShoeAndSneakers,
+  fasionShoeAndSneakers2,
+  femaleSneakersRun,
+  newPairWhiteSne,
+  pairWork,
+  unbrandedModern,
+  birthdayWatch,
+  collectionWatch,
+  collectionWatches,
+  femaleWatch,
+  pocketWatch,
+  watchShow,
+  acessoryTraditional,
+  blackToteBag,
+  capciusFemalePurse,
+  cottonToteBags,
+  eleganceYellowBag,
+  femaleHandBag,
+  leatherHandBag,
   brownHat,
   jacket,
   rolexwatches,
