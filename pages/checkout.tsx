facc4d57a3cea928569a4c5d3e6b3e6cb57adb3a
jafar <jafar.fahimi12@gmail.com>
2023-02-26 22:56:26 +0430

    checkout.tsx redirectToCheckout change, tbody

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -4,6 +4,10 @@ import { useDispatch, useSelector } from "react-redux";
 import getStripe from "utils/get-stripe";
 import { ItemPropsType } from "utils/types";
 import axios from "axios";
+import React from "react";
+// const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string);
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render. // don't need; we have getStripe
 
 type selectorType = { cartItems: ItemPropsType[]; totalPrice: number };
 type stateItemType = { cartItems: ItemPropsType[]; totalPrice: number; totalItems: number };
@@ -11,19 +15,17 @@ type stateItemType = { cartItems: ItemPropsType[]; totalPrice: number; totalItem
 let cartItems2: ItemPropsType[];
 
 const redirectToCheckout = async () => {
-  // Create Stripe checkout
-  const {
-    data: { id },
-  } = await axios.post("/api/checkout_sessions", {
-    items: Object.entries(cartItems2).map(([_, { id, qty }]) => ({
-      price: id,
-      qty,
-    })),
-  });
-
-  // Redirect to checkout
-  const stripe = await getStripe();
-  await stripe.redirectToCheckout({ sessionId: id });
+  try {
+    const stripe = await getStripe();
+    const {
+      data: { id },
+    } = await axios.post("/api/checkout_sessions", {
+      items: cartItems2,
+    });
+    stripe.redirectToCheckout({ sessionId: id });
+  } catch (err: any) {
+    console.log("err", err.message);
+  }
 };
 
 export default function Checkout() {
@@ -33,48 +35,61 @@ export default function Checkout() {
   );
   cartItems2 = itemStateArray;
 
+  React.useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+    if (query.get("success")) {
+      console.log("Order placed! You will receive an email confirmation.");
+    }
+    if (query.get("canceled")) {
+      console.log("Order canceled -- continue to shop around and checkout when you’re ready.");
+    }
+  }, []);
+
   return (
     <section className="max-w-4xl mt-8 mx-auto">
       <table>
-        <tr className="text-lg flex space-x-32 border-b-2 border-black/30 pb-3 mb-4">
-          <td className="w-36">Product</td>
-          <td>Description</td>
-          <td>Price</td>
-          <td>Quantity</td>
-          <td>Remove</td>
-        </tr>
-        {itemStateArray.map((item) => (
-          <tr
-            className="text-xl flex space-x-32 border-b-2 border-black/30 items-center pb-3 mb-3"
-            key={item.id}
-          >
-            <td className="w-36">
-              <Image src={item.imageUrl} width={150} height={180} alt={item.name} />
-            </td>
-            <td className="text-2xl w-32">{item.name}</td>
-            <td>{item.price}$</td>
-            <td className="flex items-end">
-              <button
-                onClick={() => dispatch(minusFromCart(item))}
-                className="font-extrabold text-2xl focus:ring-4"
-              >
-                &lt;
-              </button>
-              &nbsp;{item.qty}&nbsp;
-              <button
-                onClick={() => dispatch(addToCart(item))}
-                className="font-extrabold text-2xl focus:ring-4"
-              >
-                &gt;
-              </button>
-            </td>
-            <td>
-              <button className="text-2xl" onClick={() => dispatch(deleteFromCart(item))}>
-                X
-              </button>
-            </td>
+        <tbody>
+          <tr className="text-lg flex space-x-32 border-b-2 border-black/30 pb-3 mb-4">
+            <td className="w-36">Product</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Quantity</td>
+            <td>Remove</td>
           </tr>
-        ))}
+          {itemStateArray.map((item) => (
+            <tr
+              className="text-xl flex space-x-32 border-b-2 border-black/30 items-center pb-3 mb-3"
+              key={item.id}
+            >
+              <td className="w-36">
+                <Image src={item.imageUrl} width={150} height={180} alt={item.name} />
+              </td>
+              <td className="text-2xl w-32">{item.name}</td>
+              <td>{item.price}$</td>
+              <td className="flex items-end">
+                <button
+                  onClick={() => dispatch(minusFromCart(item))}
+                  className="font-extrabold text-2xl focus:ring-4"
+                >
+                  &lt;
+                </button>
+                &nbsp;{item.qty}&nbsp;
+                <button
+                  onClick={() => dispatch(addToCart(item))}
+                  className="font-extrabold text-2xl focus:ring-4"
+                >
+                  &gt;
+                </button>
+              </td>
+              <td>
+                <button className="text-2xl" onClick={() => dispatch(deleteFromCart(item))}>
+                  X
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
       <div className="flex flex-col items-end">
         <span className="uppercase text-3xl">Total: {totalPrice}$</span>
