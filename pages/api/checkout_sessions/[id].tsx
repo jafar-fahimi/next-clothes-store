127133036334d4b880ad3b5cebaea07cdc23e565
jafar <jafar.fahimi12@gmail.com>
2023-02-26 14:43:27 +0430

    checkout_sessions/[id].tsx coded

---
+++ pages/api/checkout_sessions/[id].tsx
@@ -0,0 +1,20 @@
+// to retrieve d session using session id
+import { NextApiRequest, NextApiResponse } from "next";
+import Stripe from "stripe";
+
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { id } = req.query;
+
+  try {
+    if (typeof id == "string" && !id.startsWith("cs_")) {
+      throw Error("Incorrect CheckoutSession ID.");
+    }
+    const checkout_session = await stripe.checkout.sessions.retrieve(id);
+    res.status(200).json(checkout_session);
+  } catch (err) {
+    res.status(500).json({ statusCode: 500, message: (err as unknown as Error).message });
+  }
+}
