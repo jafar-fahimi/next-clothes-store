c5de85cc86398900e03aad0af14aefacb8bdd572
jafar <jafar.fahimi12@gmail.com>
2023-02-26 22:53:59 +0430

    allCatagories.tsx 2 div -> 1

--- components/catagory/allCatagories.tsx
+++ components/catagory/allCatagories.tsx
@@ -6,13 +6,11 @@ export default function AllCatagories() {
   return (
     <section className="w-full">
       <div className="flex flex-col items-center w-full flex-wrap sm:flex-row justify-center gap-x-4 gap-y-4 mb-2">
-        <Catagory srcProp={brownHat} cat="Hats" />
-        <Catagory srcProp={jacket} cat="Jackets" />
-        <Catagory srcProp={sneakers} cat="Sneakers" />
-      </div>
-      <div className="flex flex-col items-center w-full flex-wrap sm:flex-row justify-center gap-x-4 gap-y-4">
-        <Catagory srcProp={rolexwatches} cat="watches" />
-        <Catagory srcProp={bagCollection} cat="bags" />
+        <Catagory srcProp={brownHat} cat="hats" />
+        <Catagory srcProp={jacket} cat="jackets" />
+        <Catagory srcProp={sneakers} cat="sneakers" />
+        <Catagory srcProp={rolexwatches} cat="watch" />
+        <Catagory srcProp={bagCollection} cat="bag" />
       </div>
     </section>
   );
