63f87996744d3276122543ab1d1ae5c3ba3d144c
jafar <jafar.fahimi12@gmail.com>
2023-02-27 10:49:00 +0430

    [id] -> [sessionId].tsx

--- pages/api/checkout_sessions/[sessionId].tsx
+++ pages/api/checkout_sessions/[sessionId].tsx
@@ -1,4 +1,4 @@
-// to retrieve d session using session id
+// to retrieve d session using sessionId
 import { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
 
@@ -6,13 +6,14 @@ import Stripe from "stripe";
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { id } = req.query;
+  const { sessionId } = req.query;
+  console.log("sessionId is", sessionId);
 
   try {
-    if (typeof id == "string" && !id.startsWith("cs_")) {
-      throw Error("Incorrect CheckoutSession ID.");
+    if (typeof sessionId == "string" && !sessionId.startsWith("cs_")) {
+      throw Error("Incorrect CheckoutSession sessionId.");
     }
-    const checkout_session = await stripe.checkout.sessions.retrieve(id);
+    const checkout_session = await stripe.checkout.sessions.retrieve(sessionId);
     res.status(200).json(checkout_session);
   } catch (err) {
     res.status(500).json({ statusCode: 500, message: (err as unknown as Error).message });
