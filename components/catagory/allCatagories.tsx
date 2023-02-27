a975fafc34562e8e8f4a0f1f39f70a26087665e8
jafar <jafar.fahimi12@gmail.com>
2023-02-27 08:09:33 +0430

    allCatagories.tsx hat img changed to hatsCollection

--- components/catagory/allCatagories.tsx
+++ components/catagory/allCatagories.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Catagory from "./catagory";
-import { brownHat, rolexwatches, bagCollection, jacket, sneakers } from "public/images";
+import { rolexwatches, bagCollection, jacket, sneakers, hatsCollection } from "public/images";
 
 export default function AllCatagories() {
   return (
     <section className="w-full">
       <div className="flex flex-col items-center w-full flex-wrap sm:flex-row justify-center gap-x-4 gap-y-4 mb-2">
-        <Catagory srcProp={brownHat} cat="hats" />
+        <Catagory srcProp={hatsCollection} cat="hats" />
         <Catagory srcProp={jacket} cat="jackets" />
         <Catagory srcProp={sneakers} cat="sneakers" />
         <Catagory srcProp={rolexwatches} cat="watch" />
