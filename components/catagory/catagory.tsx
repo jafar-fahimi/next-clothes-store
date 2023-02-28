b3ae037ae20d397d91f7425e5f92d75596470b94
jafar <jafar.fahimi12@gmail.com>
2023-02-28 11:36:16 +0430

    catagory.tsx border added

--- components/catagory/catagory.tsx
+++ components/catagory/catagory.tsx
@@ -9,15 +9,14 @@ export default function Catagory({ srcProp, cat }: Props) {
   if (cat == "bags") cat2 = "bag";
 
   return (
-    <Link className="relative" href={`/${!cat2 ? cat.toLowerCase() : cat2}`}>
-      <div className="w-96 -z-10 group">
+    <Link className="relative border-slate-100 border-2" href={`/${!cat2 ? cat.toLowerCase() : cat2}`}>
+      <div className="w-96 h-60 -z-10 group">
         <Image
           alt="Catagory Item Image"
           className="group-hover:scale-105 transition-all w-full h-full duration-500 flex-1"
           src={srcProp}
-          quality={100}
-          width={100}
-          height={80}
+          width={300}
+          height={240}
         />
         <div className="absolute hover:cursor-pointer -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-40 bg-white/70 hover:bg-white/90 transition-all duration-300 py-4 px-2 text-center">
           <h3 className="md:text-xl">{cat}</h3>
