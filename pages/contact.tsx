7111b6a3fb1d78e3eed2b486c27dd07007eed9cd
jafar <jafar.fahimi12@gmail.com>
2023-02-28 07:53:32 +0430

    contact.tsx sendDataHandler coded

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-// import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { TextareaHTMLAttributes, useRef, useState } from "react";
 import { Switch } from "@headlessui/react";
 
 function classNames(...classes: any) {
@@ -7,8 +6,35 @@ function classNames(...classes: any) {
 }
 
 export default function Example() {
+  const sendDataHandler = async () => {
+    try {
+      const result = await fetch("/api/message", {
+        method: "POST",
+        body: JSON.stringify({
+          name: nameRef.current?.value,
+          email: emailRef.current?.value,
+          phone: phoneRef.current?.value,
+          message: messageRef.current?.value,
+          company: companyRef.current?.value,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      alert(`result is ${result}`);
+    } catch (err) {
+      console.log("err ", err);
+      console.log("err.message ", err?.message);
+      alert("err.message is : ", err);
+    }
+  };
   const [agreed, setAgreed] = useState(false);
-
+  const nameRef = useRef<HTMLInputElement>(null);
+  const lastNameRef = useRef<HTMLInputElement>(null);
+  const phoneRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const messageRef = useRef<HTMLInputElement>(null);
+  const companyRef = useRef<HTMLInputElement>(null);
   return (
     <section className="isolate bg-white py-4 px-6 sm:py-12 lg:px-8">
       {/* <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
@@ -50,9 +76,11 @@ export default function Example() {
             </label>
             <div className="mt-2.5">
               <input
+                ref={nameRef}
                 type="text"
                 name="first-name"
                 id="first-name"
+                required
                 autoComplete="given-name"
                 className="block w-full rounded-md border-0 py-2 px-3.5 text-sm leading-6 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
               />
@@ -65,8 +93,10 @@ export default function Example() {
             <div className="mt-2.5">
               <input
                 type="text"
+                ref={lastNameRef}
                 name="last-name"
                 id="last-name"
+                required
                 autoComplete="family-name"
                 className="block w-full rounded-md border-0 py-2 px-3.5 text-sm leading-6 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
               />
@@ -74,13 +104,14 @@ export default function Example() {
           </div>
           <div className="sm:col-span-2">
             <label htmlFor="company" className="block text-sm font-semibold leading-6 text-gray-900">
-              Company
+              Company <span className="text-slate-400 text-xs">(optional)</span>
             </label>
             <div className="mt-2.5">
               <input
                 type="text"
                 name="company"
                 id="company"
+                ref={companyRef}
                 autoComplete="organization"
                 className="block w-full rounded-md border-0 py-2 px-3.5 text-sm leading-6 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
               />
@@ -94,6 +125,8 @@ export default function Example() {
               <input
                 type="email"
                 name="email"
+                required
+                ref={emailRef}
                 id="email"
                 autoComplete="email"
                 className="block w-full rounded-md border-0 py-2 px-3.5 text-sm leading-6 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
@@ -127,6 +160,8 @@ export default function Example() {
                 type="tel"
                 name="phone-number"
                 id="phone-number"
+                required
+                ref={phoneRef}
                 autoComplete="tel"
                 className="block w-full rounded-md border-0 py-2 px-3.5 pl-20 text-sm leading-6 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
               />
@@ -140,13 +175,15 @@ export default function Example() {
               <textarea
                 name="message"
                 id="message"
+                required
+                ref={messageRef}
                 rows={4}
                 className="block w-full rounded-md border-0 py-2 px-3.5 text-sm leading-6 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
                 defaultValue={""}
               />
             </div>
           </div>
-          <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
+          {/* <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
             <div className="flex h-6 items-center">
               <Switch
                 checked={agreed}
@@ -173,10 +210,11 @@ export default function Example() {
               </a>
               .
             </Switch.Label>
-          </Switch.Group>
+          </Switch.Group> */}
         </div>
         <div className="mt-10">
           <button
+            onClick={sendDataHandler}
             type="submit"
             className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
