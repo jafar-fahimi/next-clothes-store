764d51166e2ff44f32b2f91fe431e8c620b55418
jafar <jafar.fahimi12@gmail.com>
2023-02-28 09:08:28 +0430

    message.ts try catch added

--- pages/api/message.ts
+++ pages/api/message.ts
@@ -1,20 +1,46 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { connectDatabase, insertData } from "utils/db-utils";
+import { connectDatabase, getAllData, insertData } from "utils/db-utils";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method == "POST") {
-    const data = req.body;
-    const client = await connectDatabase();
-    const result = await insertData(client, "messages", {
-      id: new Date(),
-      name: data.name,
-      lastName: data.lastName,
-      email: data.email,
-      message: data.message,
-    });
-    res.status(200).json({ result });
-  } else {
-    res.setHeader("Allow", "POST");
-    res.status(405).end("Method Not Allowed");
+  let client;
+  try {
+    client = await connectDatabase();
+  } catch (error) {
+    res.status(500).json({ message: "Connecting to the database failed!" });
+    return;
   }
+
+  if (req.method === "POST") {
+    const { email, name, message, lastName, phone } = req.body;
+
+    // server side validation
+    if (!email.includes("@") || !name || name.trim() === "" || !message || message.trim() === "") {
+      res.status(422).json({ message: "Invalid input." });
+      client.close();
+      return;
+    }
+    const id = new Date();
+    const newComment = { email, name, message, id, lastName, phone };
+    // mongodb will create a unique id for us
+
+    let result;
+
+    try {
+      result = await insertData(client, "messages", newComment);
+      newComment.id = result.insertedId; // not necessary
+      res.status(201).json({ message: "Message Sent.", comment: newComment });
+    } catch (error) {
+      res.status(500).json({ message: "Sending message failed!" });
+    }
+  }
+
+  if (req.method === "GET") {
+    try {
+      const documents = await getAllData(client, "messages", { _id: -1 });
+      res.status(200).json({ comments: documents });
+    } catch (error) {
+      res.status(500).json({ message: "Getting Messages failed." });
+    }
+  }
+  client.close();
 }
