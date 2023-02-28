74586a5b0604b841f49bb682974fa3bf0567593f
jafar <jafar.fahimi12@gmail.com>
2023-02-28 11:05:53 +0430

    mongodb.ts <Res> fixed

--- pages/api/mongodb.ts
+++ pages/api/mongodb.ts
@@ -1,3 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import Stripe from "stripe";
+
+type Res = {
+  session?: Stripe.Checkout.Session;
+  message?: string;
+  statusCode?: number;
+};
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Res>) {}
