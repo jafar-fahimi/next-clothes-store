77c903535bb9af195545ce90946a0eade90b068d
jafar <jafar.fahimi12@gmail.com>
2023-02-28 11:46:25 +0430

    contact.tsx button blued

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -214,7 +214,7 @@ export default function Example() {
           <button
             onClick={sendDataHandler}
             type="submit"
-            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="block w-full rounded-md bg-blue-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
           >
             Lets talk
           </button>
